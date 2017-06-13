7038dbb561e6d81635f1c3a5da7643bc155c7a03
ahmedfaraz <ahmed.faraz0046@gmail.com>
2017-06-13 08:23:43 +0700

    with csv for file reading

--- WetherManDriver.py
+++ WetherManDriver.py
@@ -9,7 +9,8 @@ from datetime import date
 from statistics import mean
 import os.path
 import sys
-from termcolor import colored, cprint
+import csv
+from termcolor import colored
 
 from Weather import Weather
 
@@ -18,26 +19,24 @@ class WeatherManDriver:
     def _read_data_of_a_month(name):
         days = []
         if os.path.exists(sys.argv[3]+ '/' + name +'.txt'):
-            for line in open(sys.argv[3]+ '/' + name +'.txt', 'r'):
-                line_in_list_form = line.split(',')
-                date_string = line_in_list_form[0]
-                date_in_list_form = date_string.split('-')
-
-                if date_in_list_form[0] not in ('PKT', 'PKST', '\n' , '<!'): # because 1st two lines and last line of file are not valid
-                    date_obj = date(year=int(date_in_list_form[0]), month=int(date_in_list_form[1]), day=int(date_in_list_form[2]))
-                    max_t = line_in_list_form[1]
-                    mean_t = line_in_list_form[2]
-                    min_t = line_in_list_form[3]
-                    dew_point = line_in_list_form[4]
-                    mean_dew = line_in_list_form[5]
-                    min_dew = line_in_list_form[6]
-                    max_humadity = line_in_list_form[7]
-                    mean_humadity = line_in_list_form[8]
-                    min_humadity = line_in_list_form[9]
-
-                    weather = Weather(date_obj, max_t, mean_t,min_t, dew_point, mean_dew, min_dew
-                                      , max_humadity, mean_humadity, min_humadity)
-                    days.append(weather)
+            with open(sys.argv[3] + '/' + name + '.txt', 'r') as f:
+                reader = csv.reader(f)
+                for i, line in enumerate(reader):
+                    if i>1 and line[0] != '<!-- 0.190:0 -->':
+                        date_string = line[0]
+                        date_in_list_form = date_string.split('-')
+                        date_obj = date(year=int(date_in_list_form[0]), month=int(date_in_list_form[1]), day=int(date_in_list_form[2]))
+                        max_t = line[1]
+                        mean_t = line[2]
+                        min_t = line[3]
+                        dew_point = line[4]
+                        mean_dew = line[5]
+                        min_dew = line[6]
+                        max_humadity = line[7]
+                        mean_humadity = line[8]
+                        min_humadity = line[9]
+                        weather = Weather(date_obj, max_t, mean_t,min_t, dew_point, mean_dew, min_dew, max_humadity, mean_humadity, min_humadity)
+                        days.append(weather)
         else:
             print ('Searched File:'+sys.argv[3]+ '/' + name +'.txt')
             print("Error: Data file related to your query doesn\'t exists ..")
